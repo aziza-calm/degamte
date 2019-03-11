3c068625697d0b784d3856a3463d317ee49861fe
Aziza Kalmasova <aziza.kalmasova@phystech.edu>
2019-03-11 13:53:19 +0300

    blablabla

--- funny_test.py
+++ funny_test.py
@@ -2,7 +2,18 @@ import tkinter as tk
 import tkinter.ttk
 from tkinter import messagebox
 import os
+import sys
 import time
+import webbrowser
+
+def goopage(root):
+	webbrowser.open('https://accounts.google.com')
+	bBegin = tk.Button(root, width=15, height=1, text="Start Test")
+	bBegin.grid(row=2, column=0)
+	
+
+def show_readme():
+	os.system("gedit README.md")
 
 root = tk.Tk()
 root.geometry("500x200+70+60")
@@ -13,10 +24,10 @@ text.grid(row=0, column=0, columnspan=2)
 text.insert('1.0', "Hallo!\n")
 text.insert('end', "Sie beginnen den Test. Sind Sie bereit?")
 
-bJa = tk.Button(root, width=15, height=1, text="Ja! Natürlich!")
+bJa = tk.Button(root, width=15, height=1, text="Ja! Natürlich!", command=lambda: goopage(root))
 bJa.grid(row=1, column=0)
 
-bWas = tk.Button(root, width=15, height=1, text="Was ist hier los?")
+bWas = tk.Button(root, width=15, height=1, text="Was ist hier los?", command=lambda: show_readme())
 bWas.grid(row=1, column=1)
 
 root.mainloop()
